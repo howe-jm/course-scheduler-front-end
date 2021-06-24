a9a41cae6d7f6084416d21da6117740cf4263068
Jason Howe <trisynth@gmail.com>
2021-06-24 13:48:51 -0400

    Adds student schedule component to schedule view.

--- src/components/SingleStudentSchedule/SingleStudentScheduleComponent.vue
+++ src/components/SingleStudentSchedule/SingleStudentScheduleComponent.vue
@@ -1,10 +1,23 @@
 <template>
-  <div v-if="this.scheduleComponent">
-    {{
-      scheduleComponent.schedule.course.course_code +
-      ": " +
-      scheduleComponent.schedule.course.subject
-    }}
+  <div v-if="this.scheduleComponent" class="schedule-component-inner">
+    <div class="course-name">
+      {{
+        scheduleComponent.schedule.course.course_code +
+        ": " +
+        scheduleComponent.schedule.course.subject
+      }}
+    </div>
+    <div class="course-days">{{ daysString }}</div>
+    <div class="course-times">
+      {{
+        scheduleComponent.schedule.start_time +
+        " - " +
+        scheduleComponent.schedule.end_time
+      }}
+    </div>
+    <div class="course-professor">
+      {{ "Prof. " + scheduleComponent.schedule.professor.last_name }}
+    </div>
   </div>
   <div v-else>Schedule is loading...</div>
 </template>
@@ -17,5 +30,57 @@ export default {
   mounted() {
     console.log(this.scheduleComponent);
   },
+  computed: {
+    daysString() {
+      let { monday, tuesday, wednesday, thursday, friday } =
+        this.scheduleComponent.schedule;
+      let daysArray = [];
+      if (monday) {
+        daysArray.push("Monday");
+      }
+      if (tuesday) {
+        daysArray.push("Tuesday");
+      }
+      if (wednesday) {
+        daysArray.push("Wednesday");
+      }
+      if (thursday) {
+        daysArray.push("Thursday");
+      }
+      if (friday) {
+        daysArray.push("Friday");
+      }
+
+      return daysArray.join(", ");
+    },
+  },
 };
 </script>
+
+<style scoped>
+.schedule-component-inner {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border-bottom: 1px solid black;
+  border-left: 1px solid black;
+  border-right: 1px solid black;
+  width: fit-content;
+  margin: auto;
+}
+.course-name {
+  border-right: 1px solid black;
+  width: 350px;
+}
+.course-days {
+  width: 200px;
+}
+.course-times {
+  width: 150px;
+  border-left: 1px solid black;
+}
+.course-professor {
+  width: 150px;
+  border-left: 1px solid black;
+}
+</style>
